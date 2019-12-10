a2adafb62e1034c1a9c2412de3936a9627140d16
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2019-12-10 21:45:29 +0100

    fixed function name and added input check

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,7 @@
+import re
+import sys
+
+
+if not re.match(r'^[_a-zA-Z][_a-zA-Z0-9]+$', '{{ cookiecutter.project_name }}'):
+    print("ERROR: invalid project name. Must start with a letter followed by letter, digits, underscores or dashs")
+    sys.exit(1)
