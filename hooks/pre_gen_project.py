2454f546327746356bc84791bc9f569e08085c98
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2019-12-11 13:59:22 +0100

    improvements

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,6 +2,6 @@ import re
 import sys
 
 
-if not re.match(r'^[_a-zA-Z][_a-zA-Z0-9]+$', '{{ cookiecutter.project_name }}'):
+if not re.match(r'^[_a-zA-Z][\-_a-zA-Z0-9]+$', '{{ cookiecutter.project_name }}'):
     print("ERROR: invalid project name. Must start with a letter followed by letter, digits, underscores or dashs")
     sys.exit(1)
